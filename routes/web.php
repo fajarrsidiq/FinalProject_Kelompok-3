61a5c6a13de7179c9176a61ccded9b209162e229
Mahou666 <zahranalfikri31@gmail.com>
2026-05-14 21:32:33 +0700

    menambahkan routes kategori

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('barang', BarangController::class);
 });
 
 
