36337322454eb1284c771301116b8238c486ffd5
Mahou666 <zahranalfikri31@gmail.com>
2026-05-14 21:27:36 +0700

    membuat model kategori barang

---
+++ app/Models/KategoriBarang.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriBarang extends Model
+{
+    protected $table = 'kategori_barang';
+    protected $fillable = ['nama_kategori', 'deskripsi'];
+}
