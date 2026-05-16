3449704a0c33fa8cabd90d686e31bd03e8e1c80a
Ikhsnmlnn <ikhsnmlnn@gmail.com>
2026-05-16 18:51:50 +0700

    Test

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
